428d47d3c1c46222215038b084d500159b7bb77b
simco-gabriela <gabrielasimco@yahoo.com>
2024-10-16 15:44:45 +0300

    score animation

--- tema 2/Assets/Scripts/BallHole.cs
+++ tema 2/Assets/Scripts/BallHole.cs
@@ -5,13 +5,17 @@ using UnityEngine;
 public class GolfBall : MonoBehaviour
 {
     private Vector3 resetPosition = new Vector3(3, 0.5f, 2);
+    public ParticleSystem particleSystem;
 
     void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Hole"))
         {
             scoreManager.Instance.AddPoint();
-
+            if (particleSystem != null)
+            {
+                particleSystem.Play();
+            }
             transform.position = resetPosition;
         }
     }
