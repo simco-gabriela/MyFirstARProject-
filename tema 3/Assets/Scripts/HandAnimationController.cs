86ea8a6e67118903a2e4b1922814cfb1b42e97bb
simco-gabriela <gabrielasimco@yahoo.com>
2024-10-25 21:09:54 +0300

    final update

--- tema 3/Assets/Scripts/HandAnimationController.cs
+++ tema 3/Assets/Scripts/HandAnimationController.cs
@@ -1,12 +1,39 @@
 using UnityEngine;
+using UnityEngine.XR;
 using UnityEngine.XR.Interaction.Toolkit;
 
-public class HandAnimationController : MonoBehaviour
+public class HandController : MonoBehaviour
 {
-   
+    public ActionBasedController handController;
+    private Animator animator;
 
-    void Update()
+    private void Awake()
     {
-      
+        animator = GetComponent<Animator>();
+
+        if (!handController)
+        {
+            Debug.LogError("Hand controller not assigned.");
+        }
+    }
+
+    private void Update()
+    {
+        HandleInput(handController);
+    }
+
+    private void HandleInput(ActionBasedController controller)
+    {
+        if (controller)
+        {
+            bool isGripPressed = controller.selectAction.action.ReadValue<float>() > 0.0f;
+            animator.SetBool("isGrabbing", isGripPressed);
+
+            bool isTriggerPressed = controller.activateAction.action.ReadValue<float>() > 0.0f;
+            animator.SetBool("isTrigger", isTriggerPressed);
+
+            bool isPeaceSignPressed = Input.GetKeyDown(KeyCode.N);
+            animator.SetBool("isPeaceSign", isPeaceSignPressed);
+        }
     }
 }
